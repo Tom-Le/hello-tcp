755b115bdb4e57e1b9f6dbb9eb2be658dd99a6f9
Tom Le <github@leson.dev>
2024-07-26 15:10:31 -0700

    Write an empty HTTP response

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use std::{
-  io::{BufRead, BufReader},
+  io::{BufRead, BufReader, Write},
   net::{TcpListener, TcpStream},
 };
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
   println!("Connection established!");
 
   let buf_reader = BufReader::new(&mut stream);
@@ -14,15 +14,19 @@ fn handle_connection(mut stream: TcpStream) {
     .collect();
 
   println!("Request: {http_request:#?}");
+
+  let response = "HTTP/1.1 200 OK\r\n\r\n";
+  stream.write_all(response.as_bytes())?;
+
+  Ok(())
 }
 
 pub fn run() -> anyhow::Result<()> {
   let listener = TcpListener::bind("127.0.0.1:7878")?;
 
   for stream in listener.incoming() {
-    match stream {
-      Ok(stream) => handle_connection(stream),
-      Err(e) => println!("Connection failed: {e}"),
+    if let Err(e) = stream.map(handle_connection) {
+      println!("Failed to handle connection: {e}");
     }
   }
 
