84896b3876317eca8508e0a3fbc1e84376f3f4e0
Tom Le <github@leson.dev>
2024-07-26 15:35:16 -0700

    Write a real HTTP response with HTML

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::{
+  fs,
   io::{BufRead, BufReader, Write},
   net::{TcpListener, TcpStream},
 };
@@ -15,7 +16,11 @@ fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
 
   println!("Request: {http_request:#?}");
 
-  let response = "HTTP/1.1 200 OK\r\n\r\n";
+  let status_line = "HTTP/1.1 200 OK";
+  let contents = fs::read_to_string("hello.html")?;
+  let length = contents.len();
+
+  let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
   stream.write_all(response.as_bytes())?;
 
   Ok(())
@@ -25,7 +30,10 @@ pub fn run() -> anyhow::Result<()> {
   let listener = TcpListener::bind("127.0.0.1:7878")?;
 
   for stream in listener.incoming() {
-    if let Err(e) = stream.map(handle_connection) {
+    let result = stream
+      .map_err(anyhow::Error::new)
+      .and_then(handle_connection);
+    if let Err(e) = result {
       println!("Failed to handle connection: {e}");
     }
   }
